90a9ae06386e12a1fbb468ce32520f1b9f626f97
Amy Roberts <aroberts@live.unc.edu>
2013-07-29 18:53:25 -0400

    added from bitstarter

---
+++ web.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+var express = require('express');
+
+var app = express.createServer(express.logger());
+var buf = fs.readFileSync('index.html', 'utf-8');
+var string =buf.toString();
+app.get('/', function(request, response)  {
+  response.send(string);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
